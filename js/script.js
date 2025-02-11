7c7871a1cebbc55dcc739432b9c0d4e2360a220a
Vitalii Tomenko <vitalikxxx985@gmail.com>
2025-02-11 18:09:31 +0100

    Add function to send user messages

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const inputBox = document.querySelector(".chat__input");
+const chatBox = document.querySelector(".chat__box");
+const sendButton = document.querySelector(".chat__button");
+
+const sendMessage = () => {
+    const messag = inputBox.value.trim();
+    if (!messag) return;
+
+
+};
